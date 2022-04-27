76ee8fd367e32a11cd6d25cf97da994b74cd398c
MiltonPonceRodriguez1 <miltonponceipn@gmail.com>
2022-04-27 00:46:55 -0500

    changes-service

--- frontend/src/app/components/motorcycles-manage/motorcycles-manage.component.ts
+++ frontend/src/app/components/motorcycles-manage/motorcycles-manage.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {DataTablesModule} from 'angular-datatables';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClient } from "@angular/common/http";
+import { MotorcycleService } from "../../services/motorcycle.service";
 
 declare var jQuery:any;
 declare var $:any;
@@ -11,50 +12,21 @@ declare var $:any;
 @Component({
   selector: 'app-motorcycles-manage',
   templateUrl: './motorcycles-manage.component.html',
-  styleUrls: ['./motorcycles-manage.component.css']
+  styleUrls: ['./motorcycles-manage.component.css'],
+  providers: [MotorcycleService]
 })
 export class MotorcyclesManageComponent implements OnInit {
   public data: any;
 
   constructor(
+    private _motorcycleService: MotorcycleService,
     private http: HttpClient
-  ) {
-    this.http.get('https://jsonplaceholder.typicode.com/todos').subscribe(data => {
-
-      this.data = data;
-      console.log(this.data);
-      setTimeout(()=>{
-        $('#datatableexample').DataTable({
-          pagingType: 'full_numbers',
-          pageLength: 5,
-          processing: true,
-          lengthMenu : [5, 10, 25],
-          /*language: {
-              "decimal": "",
-              "emptyTable": "No hay información",
-              "info": "Mostrando START a END de TOTAL Entradas",
-              "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
-              "infoFiltered": "(Filtrado de MAX total entradas)",
-              "infoPostFix": "",
-              "thousands": ",",
-              "lengthMenu": "Mostrar MENU Entradas",
-              "loadingRecords": "Cargando...",
-              "processing": "Procesando...",
-              "search": "Buscar:",
-              "zeroRecords": "Sin resultados encontrados",
-              "paginate": {
-                  "first": "Primero",
-                  "last": "Ultimo",
-                  "next": "Siguiente",
-                  "previous": "Anterior"
-              }
-          }*/
-        });
-      }, 1);
-    }, error => console.error(error));
-  }
+  ) {  }
 
   ngOnInit(): void {
+    console.log("ENTRO XD");
+    this.data = this._motorcycleService.index();
+    console.log(this.data);
   }
 
 }
--- frontend/src/app/services/motorcycle.service.ts
+++ frontend/src/app/services/motorcycle.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpClientModule } from '@angular/common/http';
+import {DataTablesModule} from 'angular-datatables';
 import { Observable } from "rxjs";
 import { Motorcycle } from '../models/motorcycle';
 import { global } from "./global";
+
+declare var jQuery:any;
+declare var $:any;
 
 @Injectable()
 export class MotorcycleService {
 
     public url: string;
+    public data: any;
 
     constructor(
         public _http: HttpClient
@@ -19,6 +24,47 @@ export class MotorcycleService {
         return "Hola desde el servicio motorcycle";
     }
 
+    index(): Observable<any> {
+        let headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+        //return this._http.get(this.url + 'motorcycle/index', {headers: headers});
+
+        this._http.get(this.url + 'motorcycle/index').subscribe(data => {
+
+        this.data = data;
+
+        setTimeout(()=>{
+            $('#datatableexample').DataTable({
+            pagingType: 'full_numbers',
+            pageLength: 5,
+            processing: true,
+            lengthMenu : [5, 10, 25],
+            /*language: {
+                "decimal": "",
+                "emptyTable": "No hay información",
+                "info": "Mostrando START a END de TOTAL Entradas",
+                "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
+                "infoFiltered": "(Filtrado de MAX total entradas)",
+                "infoPostFix": "",
+                "thousands": ",",
+                "lengthMenu": "Mostrar MENU Entradas",
+                "loadingRecords": "Cargando...",
+                "processing": "Procesando...",
+                "search": "Buscar:",
+                "zeroRecords": "Sin resultados encontrados",
+                "paginate": {
+                    "first": "Primero",
+                    "last": "Ultimo",
+                    "next": "Siguiente",
+                    "previous": "Anterior"
+                }
+            }*/
+            });
+        }, 1);
+        }, error => console.error(error));
+
+        return this.data;
+    }
+
     add_motorcycle(motorcycle: Motorcycle): Observable<any> {
         let json = JSON.stringify(motorcycle);
         let params = 'json='+json;
@@ -28,7 +74,6 @@ export class MotorcycleService {
         let headers = new HttpHeaders().set("Content-Type", "application/x-www-form-urlencoded");
 
         return this._http.post(this.url+'motorcycle/store', params, {headers: headers});
-
     }
 
 }
